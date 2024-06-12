4e46c8d2bd32b9e11309f3d182b998354a0b0277
Alang_laptop <Alang@laptop.com>
2024-06-12 20:40:09 +0800

    修改侧边栏菜单

--- src/app/shell/shell.component.ts
+++ src/app/shell/shell.component.ts
@@ -4,6 +4,7 @@ import { RouterOutlet } from '@angular/router';
 import { ShareModule } from '../shared/modules/share.module';
 import { EnvironmentService } from '../services/environment.service';
 import { LoginService } from '../services/login.service';
+import { WindowService } from '../services/window.service';
 
 @Component({
   selector: 'app-shell',
@@ -15,9 +16,32 @@ import { LoginService } from '../services/login.service';
 export class ShellComponent {
   env = inject(EnvironmentService);
   loginService = inject(LoginService);
+  windowService = inject(WindowService);
   isCollapsed = false;
   title = browserTitle;
   logout() {
     this.loginService.logout();
   }
+  name: string | null =
+    this.windowService.getSessionStorage('name') ||
+    this.windowService.getStorage('name');
+  role: string | null =
+    this.windowService.getSessionStorage('role') ||
+    this.windowService.getStorage('role');
+  menus: any = {
+    U: [
+      {
+        name: '心理评估',
+        url: 'new-evaluation',
+        icon: 'user',
+      },
+    ],
+    A: [
+      {
+        name: '用户管理',
+        url: 'user-admin',
+        icon: 'user',
+      },
+    ],
+  };
 }
